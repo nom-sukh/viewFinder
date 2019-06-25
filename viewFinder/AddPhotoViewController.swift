af8bf6292fbac31f90e3a42525a44ac99768410e
Apple <apple@Apples-MacBook-Pro-6.local>
2019-06-25 10:29:12 -0400

    Complete Iteration 2

--- viewFinder/AddPhotoViewController.swift
+++ viewFinder/AddPhotoViewController.swift
@@ -13,6 +13,24 @@ class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
     var imagePicker = UIImagePickerController()
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionText: UITextView!
+    @IBAction func savePhotoTapped(_ sender: UIButton)
+    {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
+        {
+            let photoToSave = Photos(entity: Photos.entity(), insertInto: context)
+            photoToSave.caption = captionText.text
+            if let userImage = imageView.image
+            {
+                if let userImageData = userImage.pngData()
+                {
+                    photoToSave.imageData = userImageData
+                }
+            }
+            (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+            navigationController?.popViewController(animated: true)
+        }
+    }
     override func viewDidLoad()
     {
         super.viewDidLoad()
